5df1536417565a7368ad9febad11859488bd273c
Michael Cuneo <me@michaelcuneo.com.au>
2025-04-17 14:32:06 +1000

    Initial Commit

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default defineConfig([
+  {
+    ignores: ["node_modules/", "dist/", "build/", "package-lock.json"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+  },
+  eslintPluginPrettierRecommended,
+]);
---
+++ src/utils.ts
@@ -0,0 +1,4 @@
+export function add(a: number, b: number): number {
+  // This function adds two numbers and returns the result
+  return a + b;
+}
